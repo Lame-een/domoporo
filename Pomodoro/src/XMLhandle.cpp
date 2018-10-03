5b19cadcf3c19aa8cce308f895161271323bc158
Lame-een <43285057+Lame-een@users.noreply.github.com>
2018-10-03 19:15:53 +0200

    Created SaveManager and SaveData class
    Created the SaveManager class handling the XML reading and writing +
    in memory saving of the presets.

--- Pomodoro/src/XMLhandle.cpp
+++ Pomodoro/src/XMLhandle.cpp
@@ -1,33 +1,207 @@
 #include "headers/XMLhandle.h"
+#include <QDebug>
 
 //WILL BE USED FOR HANDLING THE SAVING OF TIMER PRESETS
 
-/*
+void SaveManager::readName()
+{
+	QString val = reader.readElementText();
 
-	QFile file(".\\data\\data.xml");
-	if(!file.open(QFile::ReadOnly | QFile::Text))
+	curSave.name = val;
+}
+
+void SaveManager::readTime()
+{
+	while(reader.readNextStartElement())
 	{
-		qDebug() << "Cannot read file" << file.errorString();
-		exit(0);
-	}
-
-	QString ret;
-
-	QXmlStreamReader reader(&file);
-
-	if (reader.readNextStartElement()) {
-		if (reader.name() == "root"){
-			while(reader.readNextStartElement()){
-				if(reader.name() == "childA"){
-					QString s = reader.readElementText();
-					qDebug(qPrintable(s));
-				}
-				else
-					reader.skipCurrentElement();
-			}
+		if(reader.name() == "hh")
+		{
+			int val = reader.readElementText().toInt();
+			curSave.hh = val;
+		}
+		else if(reader.name() == "mm")
+		{
+			int val = reader.readElementText().toInt();
+			curSave.mm = val;
+		}
+		else if(reader.name() == "ss")
+		{
+			int val = reader.readElementText().toInt();
+			curSave.ss = val;
 		}
 		else
-			reader.raiseError(QObject::tr("Incorrect file"));
+		{
+			reader.skipCurrentElement();
+		}
+	}
+}
+
+void SaveManager::readPath()
+{
+	QString val = reader.readElementText();
+
+	curSave.path = val;
+}
+
+void SaveManager::readVolume()
+{
+	int val = reader.readElementText().toInt();
+
+	curSave.volume = val;
+}
+
+void SaveManager::readSaveFile()
+{
+	QFile file(pathToFile);
+	if(!file.open(QFile::ReadWrite | QFile::Text))
+	{
+		QMessageBox::critical(nullptr, "Error", "Unable to open save file", QMessageBox::Ok);
 	}
 
-*/
+	reader.setDevice(&file);
+
+	if(reader.readNextStartElement())
+	{
+		if(reader.name() == "saveData")	//root of the XML
+		{
+			for(int i = 0; reader.readNextStartElement(); i++)
+			{
+				if(reader.name() == "timer")
+				{
+					while(reader.readNextStartElement())
+					{
+
+						if(reader.name() == "name")
+						{
+							readName();
+						}
+						else if(reader.name() == "time")
+						{
+							readTime();
+						}
+						else if(reader.name() == "audioPath")
+						{
+							readPath();
+						}
+						else if(reader.name() == "audioVolume")
+						{
+							readVolume();
+						}
+						else
+						{
+							reader.skipCurrentElement();
+						}
+					}
+				}
+				else
+				{
+					reader.skipCurrentElement();
+				}
+				presets.push_back(curSave);
+			}
+
+
+		}
+	}
+	else
+	{
+		reader.raiseError(QObject::tr("Incorrect file"));
+	}
+	file.close();
+}
+
+void SaveManager::saveTimer(SaveData& timerData)
+{
+	writeStream.writeStartElement("timer");	//open timer element - the timer save
+	writeStream.writeTextElement("name", timerData.name);
+
+	writeStream.writeStartElement("time");	//open time element
+	writeStream.writeTextElement("hh", QString::number(timerData.hh));
+	writeStream.writeTextElement("mm", QString::number(timerData.mm));
+	writeStream.writeTextElement("ss", QString::number(timerData.ss));
+	writeStream.writeEndElement();	//close time element
+
+	writeStream.writeTextElement("audioPath", timerData.path);
+	writeStream.writeTextElement("audioVolume", QString::number(timerData.volume));
+
+	writeStream.writeEndElement(); //close timer element - the timer save
+}
+
+void SaveManager::saveData()
+{
+	QFile file(pathToFile);
+	if(!file.open(QFile::ReadWrite | QFile::Text))
+	{
+		QMessageBox::critical(nullptr, "Error", "Unable to open save file", QMessageBox::Ok);
+	}
+	file.resize(0);
+
+	writeStream.setDevice(&file);
+	writeStream.setAutoFormatting(true);
+
+	writeStream.writeStartDocument();
+	writeStream.writeStartElement("saveData");
+
+	for(int i = 0; i < presets.size(); i++)
+	{
+		saveTimer(presets[i]);
+	}
+
+	writeStream.writeEndElement();
+	writeStream.writeEndDocument();
+}
+
+void SaveManager::appendSave(QString& name, int& hh, int& mm, int& ss, QString& path, int& volume)	//ref input: name, hh, mm, ss, path, volume
+{
+	SaveData sd;
+	sd.name = name;
+	sd.hh = hh;
+	sd.mm = mm;
+	sd.ss = ss;
+	sd.path = path;
+	sd.volume = volume;
+
+	appendSave(sd);
+}
+
+void SaveManager::appendSave(QString name, int hh, int mm, int ss, QString path, int volume)	//raw input: name, hh, mm, ss, path, volume
+{
+	SaveData sd;
+	sd.name = name;
+	sd.hh = hh;
+	sd.mm = mm;
+	sd.ss = ss;
+	sd.path = path;
+	sd.volume = volume;
+
+	appendSave(sd);
+}
+
+void SaveManager::appendSave(SaveData& timerData)
+{
+	presets.push_back(timerData);
+}
+
+void SaveManager::removePreset(int& index)
+{
+	if(index < presets.size())
+	{
+		presets.erase(presets.begin() + index);	
+	}
+	else
+	{
+		QMessageBox::critical(nullptr, "Error", "Vector index out of bounds", QMessageBox::Ok);
+	}
+}
+
+void SaveManager::removePreset(int index)
+{
+	if(index < presets.size())
+	{
+		presets.erase(presets.begin() + index);
+	}
+	else
+	{
+		QMessageBox::critical(nullptr, "Error", "Vector index out of bounds", QMessageBox::Ok);
+	}
+}
--- Pomodoro/src/headers/XMLhandle.h
+++ Pomodoro/src/headers/XMLhandle.h
@@ -1,2 +1,48 @@
 #pragma once
 //#include "timer.h"
+#include <QXMLStreamReader>
+#include <QXMLStreamWriter>
+#include <QFile>
+#include <QFileInfo>
+#include <QObject>
+#include <QMessageBox>
+#include <vector>
+
+struct SaveData
+{
+	QString name;
+	int hh;
+	int mm;
+	int ss;
+	QString path;
+	int volume;
+};
+
+class SaveManager
+{
+private:
+	void readName();
+	void readTime();
+	void readPath();
+	void readVolume();
+	void saveTimer(SaveData&);
+
+	QString pathToFile = ".\\data\\data.xml";
+
+	SaveData curSave;
+
+	QXmlStreamReader reader;
+	QXmlStreamWriter writeStream;
+public:
+	void readSaveFile();
+	void saveData();
+
+	void appendSave(SaveData&);
+	void appendSave(QString& name, int& hh, int& mm, int& ss, QString& path, int& volume);
+	void appendSave(QString, int, int, int, QString, int);
+
+	void removePreset(int&);
+	void removePreset(int);
+
+	std::vector<SaveData> presets;
+};
